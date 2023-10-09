81d35925d6891523b611a693d738912b85302eb3
RafaelLS1 <sl_rafael25@hotmail.com>
2023-10-09 14:08:05 -0300

    v1

---
+++ src/test/java/br/com/rafaelima/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelima.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
